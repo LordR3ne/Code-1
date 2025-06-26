ef6ca97cf3ba20c97b7402dfb066749da160c7f3
LordR3ne <renehill170@gmail.com>
2025-06-26 16:26:50 +0200

    Update1.11

--- TheAdventureOfCarlos(Sequel)1.0/Parent.ts
+++ TheAdventureOfCarlos(Sequel)1.0/Parent.ts
@@ -6,10 +6,10 @@ namespace Adventure {
     console.log(parent.gameState);
     if (parent.gameState == true) {
         console.log("You can't get in again");
-        location.href = "TheAdventureofCarlos.html";
         alert ("You can't do it again.")
+        location.href = "TheAdventureofCarlos.html";
 
     }
     parent.gameState = true;
     console.log(parent.gameState);
-}   
+}       
